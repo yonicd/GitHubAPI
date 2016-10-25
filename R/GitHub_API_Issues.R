88ded6a7735602e4235e0c659c21d87142612d59
pumpy7 <yoonjaej@gmail.com>
2016-10-25 16:10:43 +0900

    fixed errors

--- R/GitHub_API_Issues.R
+++ R/GitHub_API_Issues.R
@@ -36,48 +36,81 @@ getIssueData=function(repository.url,token)
 cleanUpIssueData=function(issueData)
 {
 
-
-
-  issue.title=unlist(sapply(issueData,function(x){x["title"]}))
-  issue.author=unlist(sapply(issueData,function(x){x["user"]["login"]}))
-  issue.state=unlist(sapply(issueData,function(x){x["state"]}))
-  issue.labels= sapply(issueData, function(x){
-           ifelse(length(unlist(x["labels"]["name"]))!=0,paste(unlist(x["labels"]["name"]),collapse=", "),"NA")
-         }
-  )
-  issue.assignee=sapply(issueData, function(x){
-    ifelse(length(x["assignee"]["login"])!=0,x["assignee"]["login"],"NA")
+  issue.title=unlist(sapply(issueData, function(x){
+    ifelse(!is.null(unlist(x["title"])),x["title"],"NA")
   }
-  )
-  issue.milestone=sapply(issueData, function(x){
-    ifelse(length(x["milestone"])!=0,x["milestone"],"NA")
+  ))
+
+
+  issue.author=sapply(issueData,function(x){x["user"]})
+  issue.author.cleanedup=as.character(unlist(sapply(issue.author,function(x){
+    ifelse(length(x["login"])>0,paste(x["login"],collapse = ", "),"NA")
   }
-  )
+  )))
+  rm(issue.author)
 
-  issue.numComments=sapply(issueData, function(x){
-    ifelse(length(x["comments"])!=0,as.numeric(x["comments"]),"NA")
+
+  issue.state=unlist(sapply(issueData, function(x){
+    ifelse(!is.null(unlist(x["state"])),x["state"],"NA")
   }
-  )
+  ))
 
-  issue.createdDate=sapply(issueData, function(x){
-    ifelse(length(x["created_at"])!=0,x["created_at"],"NA")
+
+  issue.labels= sapply(issueData,function(x){x["labels"]})
+  issue.labels.cleanedup=as.character(unlist(sapply(issue.labels,function(x){
+  ifelse(!is.null(unlist(x["name"])),paste(x["name"],collapse = ", "),"NA")
+    }
+  )))
+  rm(issue.labels)
+
+
+
+  issue.assignee=sapply(issueData, function(x){x["assignee"]})
+  issue.assignee.cleanedup=as.character(unlist(sapply(issue.assignee,function(x){
+    ifelse(!is.null(unlist(x["login"])),paste(x["login"],collapse = ", "),"NA")
   }
-  )
+  )))
+  rm(issue.assignee)
 
-  issue.bodyMessage=sapply(issueData, function(x){
-    ifelse(length(x["body"])!=0,x["body"],"NA")
+
+
+  issue.milestone=sapply(issueData,function(x){x["milestone"]})
+  issue.milestone.cleanedup=as.character(unlist(sapply(issue.milestone,function(x){
+    ifelse(!is.null(unlist(x["title"])),paste(x["title"],collapse = ", "),"NA")
   }
-  )
+  )))
+  rm(issue.milestone)
 
 
+
+  issue.numComments=as.numeric(sapply(issueData, function(x){
+    ifelse(as.numeric(unlist(x["comments"]))!=0,as.numeric(x["comments"]),0)
+  }
+  ))
+
+
+
+  issue.createdDate=unlist(sapply(issueData, function(x){
+    ifelse(!is.null(unlist(x["created_at"])),x["created_at"],"NA")
+  }
+  ))
+
+
+
+  issue.bodyMessage=unlist(sapply(issueData, function(x){
+    ifelse(!is.null(unlist(x["body"])),x["body"],"NA")
+
+  }
+
+  ))
 
 
   return(data.frame(title=issue.title,
-                    author=issue.author,
+                    author=issue.author.cleanedup,
                     state=issue.state,
-                    labels=issue.labels,
-                    assignee=issue.assignee,
-                    milestone=issue.milestone,
+                    labels=issue.labels.cleanedup,
+                    assignee=issue.assignee.cleanedup,
+                    milestone=issue.milestone.cleanedup,
                     numComments=issue.numComments,
                     createdAt=issue.createdDate,
                     body=issue.bodyMessage,
